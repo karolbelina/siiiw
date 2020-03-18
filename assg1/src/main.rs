75afab2eaf3c7460c27826d85a29ec0e347a6250
Karol Belina <karolbelina@gmail.com>
2020-03-18 09:46:36 +0100

    Add the plotting logger

--- assg1/src/main.rs
+++ assg1/src/main.rs
@@ -15,6 +15,9 @@ struct Config {
     /// TSP instance file
     #[structopt(long = "input", short = "i", name = "TSP FILE", parse(from_os_str))]
     tsp_path: PathBuf,
+    /// Cohorter output file
+    #[structopt(long = "output", short = "o", name = "OUTPUT FILE", parse(from_os_str))]
+    output_path: Option<PathBuf>,
 }
 
 fn main() -> CliResult {
@@ -36,16 +39,23 @@ fn main() -> CliResult {
     // println!("{:?}, {:?}", discoverer.get_best_solution(), discoverer.get_best_fitness());
 
     let mut discoverer = tsp::logs::Discoverer::new();
-    let evolutionary = ea::Evolutionary::new(
-        ops::initialize::Greedy::new(&problem, 0.1),
-        ops::select::Tournament::new(6),
-        ops::crossover::OX::new(&problem, 1.0),
-        ops::mutate::Inversion::new(&problem, 0.05),
-        10000,
-        100
-    );
-    evolutionary.run(&mut vec![&mut discoverer]);
-    println!("{:?}, {:?}", discoverer.get_best_solution(), discoverer.get_best_fitness());
+    let mut cohorter = tsp::logs::Cohorter::new(5000, 100);
+    for _ in 0..10 {
+        let evolutionary = ea::Evolutionary::new(
+            ops::initialize::Random::new(&problem),
+            ops::select::Tournament::new(6),
+            ops::crossover::OX::new(&problem, 1.0),
+            ops::mutate::Inversion::new(&problem, 0.01),
+            5000,
+            100
+        );
+        evolutionary.run(&mut vec![&mut discoverer, &mut cohorter]);
+        println!("{:?}, {:?}", discoverer.get_best_solution(), discoverer.get_best_fitness());
+        cohorter.carry();
+    }
+    if let Some(path) = config.output_path {
+        cohorter.dump(&path)?;
+    }
 
     Ok(())
 }
--- assg1/src/tsp/logs.rs
+++ assg1/src/tsp/logs.rs
@@ -30,6 +30,99 @@ impl Discoverer {
     }
 }
 
+pub struct Cohorter {
+    logged: usize,
+    generation_size: usize,
+    generations: usize,
+    currents: Option<(u32, u32)>,
+    current_bests: Vec<u32>,
+    current_worsts: Vec<u32>,
+    accumulated_bests: Vec<Vec<u32>>,
+    worsts: Option<Vec<u32>>,
+}
+
+impl Log<(Vec<usize>, u32)> for Cohorter {
+    fn log(&mut self, value: &(Vec<usize>, u32)) {
+        let (_, measure) = value;
+        match &self.currents {
+            Some((current_best, current_worst)) => {
+                let best = if measure < current_best { measure } else { current_best };
+                let worst = if measure > current_worst { measure } else { current_worst };
+                self.currents = Some((*best, *worst));
+            },
+            None => self.currents = Some((*measure, *measure))
+        }
+        self.logged += 1;
+        if self.logged == self.generation_size {
+            let (current_best, current_worst) = self.currents.unwrap();
+            self.current_bests.push(current_best);
+            self.current_worsts.push(current_worst);
+            self.logged = 0;
+            self.currents = None;
+        }
+    }
+}
+
+use quicli::prelude::*;
+use std::path::PathBuf;
+
+impl Cohorter {
+    pub fn new(generation_size: usize, generations: usize) -> Cohorter {
+        Cohorter {
+            logged: 0,
+            generation_size: generation_size,
+            generations: generations,
+            currents: None,
+            current_bests: Vec::new(),
+            current_worsts: Vec::new(),
+            accumulated_bests: Vec::new(),
+            worsts: None,
+        }
+    }
+
+    pub fn carry(&mut self) {
+        assert_eq!(self.current_bests.len(), self.generations);
+        assert_eq!(self.current_worsts.len(), self.generations);
+
+        self.accumulated_bests.push(self.current_bests.drain(0..).collect());
+        self.worsts = match &self.worsts {
+            Some(worsts) => {
+                let worsts = Some(worsts.iter().zip(self.current_worsts.iter())
+                    .map(|(worst, current_worst)| {
+                        if current_worst > worst {
+                            *current_worst
+                        } else {
+                            *worst
+                        }
+                    }).collect());
+                self.current_worsts.clear();
+                worsts
+            },
+            None => Some(self.current_worsts.drain(0..).collect())
+        };
+    }
+
+    pub fn dump(&self, path: &PathBuf) -> Result<(), Error> {
+        let bests: Vec<u32> = (0..self.generations).map(|i| self.accumulated_bests.iter()
+                .min_by(|a, b| a[i].cmp(&b[i])).unwrap()[i])
+            .collect();
+        let avgs: Vec<f64> = (0..self.generations).map(|i| self.accumulated_bests.iter()
+                .map(|accumulated_best| accumulated_best[i])
+                .sum::<u32>() as f64 / self.accumulated_bests.len() as f64)
+            .collect();
+        let worsts: &Vec<u32> = &self.worsts.as_ref().unwrap();
+        let output = bests.iter().zip(avgs.iter()).zip(worsts.iter())
+            .enumerate()
+            .map(|(i, ((best, avg), worst))| {
+            format!("{};{};{};{}", i, best, avg, worst)
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+        write_to_file(&path, &output)
+    }
+}
+
 // pub struct Logger<P: Problem> {
 //     best: Option<P::Measure>,
 //     worst: Option<P::Measure>,
